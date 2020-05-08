747d5572b171bc2c4054c64fdbd3afae846c5738
EmilyLiu18 <tong9718@gmail.com>
2020-05-07 22:31:25 -0700

    modified mongodb in blogs.js

--- routes/blogs.js
+++ routes/blogs.js
@@ -18,6 +18,7 @@ router.get('/:username/:postid', function (req, res) {
 	// db initialization
 	let url = 'mongodb://localhost:27017';
 
+	// db connection
 	MongoClient.connect(url, function(err, client) {
 		let db = client.db('BlogServer');
 
@@ -30,8 +31,10 @@ router.get('/:username/:postid', function (req, res) {
   			if(err){
   				throw err;
   			}
-  			if(resContent == null)
+  			if(resContent == null){
   				res.send("Not Matched Username and Postid!");
+  			}
+
   			let parsedTitle = reader.parse(resContent.title);
   			let resTitle = writer.render(parsedTitle);
   			let parsedBody = reader.parse(resContent.body);
@@ -60,7 +63,53 @@ router.get('/:username/:postid', function (req, res) {
 })
 
 router.get('/:username', function (req, res) {
-    res.render('blogs', { username: req.params.username, id: "Hi, I am ID" });
+	// parameters 
+	let givenUsername = req.params.username;
+
+
+	// markdown initialization
+	let reader = new commonmark.Parser();
+	let writer = new commonmark.HtmlRenderer();
+
+	// db initialization
+	let url = 'mongodb://localhost:27017';
+
+	// db connection
+	MongoClient.connect(url, function(err, client) {
+		let db = client.db('BlogServer');
+
+		//Get the markdown body and title using mongodb
+ 		// Get the documents collection
+  		let collection = db.collection('Posts');
+
+  		// Find some documents with certain conditions
+  		collection.find({"username":givenUsername}).toArray(function(err, resContent) {
+  			if(err){
+  				throw err;
+  			}
+  			if(resContent == null){
+  				res.send("Not Matched Username and Postid!");
+  			}
+
+  			let resTitle = [];
+  			let resBody = [];
+
+  			for(let i = 0; i<resContent.length; i++){
+  				let parsedTitle = reader.parse(resContent[i].title);
+  				let curTitle = writer.render(parsedTitle);
+  				let parsedBody = reader.parse(resContent[i].body);
+  				let curBody = writer.render(parsedBody);
+  				resTitle.push(curTitle);
+  				resBody.push(curBody);
+  				if(i==4){
+  					break;
+  				} 
+  			}
+
+  			res.render('blogs', { username: givenUsername, title: resTitle, body: resBody });
+  			
+  		});
+
 })
 
 module.exports = router;
