13e2d4268bc7fb5f96e05c0506dfcfd76709cc8a
EmilyLiu18 <tong9718@gmail.com>
2020-05-08 16:29:57 -0700

    Updated mongodb connection

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var MongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var blogsRouter = require('./routes/blogs');
 
 var app = express();
+
+let url = 'mongodb://localhost:27017';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/blogs', blogsRouter);
 app.use('/users', usersRouter);
+
+// mongodb connection
+MongoClient.connect(url, function(err, client) {
+	app.set('client', client);
+	console.log("Connected successfully to server");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/blogs.js
+++ routes/blogs.js
@@ -19,33 +19,32 @@ router.get('/:username/:postid', function (req, res) {
 	let url = 'mongodb://localhost:27017';
 
 	// db connection
-	MongoClient.connect(url, function(err, client) {
-		let db = client.db('BlogServer');
+	let client = req.app.get('client');
+	let db = client.db('BlogServer');
 
-		//Get the markdown body and title using mongodb
- 		// Get the documents collection
-  		let collection = db.collection('Posts');
+	//Get the markdown body and title using mongodb
+	// Get the documents collection
+  	let collection = db.collection('Posts');
 
-  		// Find some documents with certain conditions
-  		collection.findOne({"username":givenUsername, "postid": givenPostid}, function(err, resContent) {
-  			if(err){
-  				throw err;
-  			}
-  			if(resContent == null){
-				res.status(404);
-				//TODO: better error page
-				res.render('error', { message: 'Username or Postid not found!', error: {status: "Error Code: 404", stack:""}});
-  			}
-			else{
-	  			let parsedTitle = reader.parse(resContent.title);
-	  			let resTitle = writer.render(parsedTitle);
-	  			let parsedBody = reader.parse(resContent.body);
-	  			let resBody = writer.render(parsedBody);
-	  			res.render('blogs', { username: givenUsername, id: givenPostid, title: resTitle, body: resBody });
-			}
-  		});
+  	// Find some documents with certain conditions
+  	collection.findOne({"username":givenUsername, "postid": givenPostid}, function(err, resContent) {
+  		if(err){
+  			throw err;
+  		}
+  		if(resContent == null){
+			res.status(404);
+			//TODO: better error page
+			res.render('error', { message: 'Username or Postid not found!', error: {status: "Error Code: 404", stack:""}});
+  		}
+		else{
+	  		let parsedTitle = reader.parse(resContent.title);
+	  		let resTitle = writer.render(parsedTitle);
+	  		let parsedBody = reader.parse(resContent.body);
+	  		let resBody = writer.render(parsedBody);
+	  		res.render('blogs', { username: givenUsername, id: givenPostid, title: resTitle, body: resBody });
+		}
+  		
 
-  		client.close();
 	});
 
 
@@ -66,47 +65,44 @@ router.get('/:username', function (req, res) {
 	let url = 'mongodb://localhost:27017';
 
 	// db connection
-	MongoClient.connect(url, function(err, client) {
-		let db = client.db('BlogServer');
+	let client = req.app.get('client');
+	let db = client.db('BlogServer');
 
-		//Get the markdown body and title using mongodb
- 		// Get the documents collection
-  		let collection = db.collection('Posts');
+	//Get the markdown body and title using mongodb
+ 	// Get the documents collection
+  	let collection = db.collection('Posts');
 
-  		// Find some documents with certain conditions
-		// db.collection.find( { $query: {}, $orderby: { age : -1 } } )
-  		collection.find({"username":givenUsername, "postid":{$gte:start}},{"sort":"postid"}).toArray(function(err, resContent) {
-  			if(err){
-  				throw err;
-  			}
-  			if(resContent.length == 0){
-				res.status(404);
-				//TODO: better error page
-				res.render('error', { message: 'Username not found or Invalid number for start query!', error: {status: "Error Code: 404", stack:""}});
-  			}
-			else{
-	  			let resTitle = [];
-	  			let resBody = [];
-				let next_id = 0;
+  	// Find some documents with certain conditions
+	// db.collection.find( { $query: {}, $orderby: { age : -1 } } )
+  	collection.find({"username":givenUsername, "postid":{$gte:start}},{"sort":"postid"}).toArray(function(err, resContent) {
+  		if(err){
+  			throw err;
+  		}
+  		if(resContent.length == 0){
+			res.status(404);
+			//TODO: better error page
+			res.render('error', { message: 'Username not found or Invalid number for start query!', error: {status: "Error Code: 404", stack:""}});
+  		}
+		else{
+	 		let resTitle = [];
+	 		let resBody = [];
+			let next_id = 0;
 
-	  			for(let i = 0; (i < 5) && (i < resContent.length); i++){
-	  				let parsedTitle = reader.parse(resContent[i].title);
-	  				let curTitle = writer.render(parsedTitle);
-	  				let parsedBody = reader.parse(resContent[i].body);
-	  				let curBody = writer.render(parsedBody);
-	  				resTitle.push(curTitle);
-	  				resBody.push(curBody);
-	  			}
+  			for(let i = 0; (i < 5) && (i < resContent.length); i++){
+  				let parsedTitle = reader.parse(resContent[i].title);
+  				let curTitle = writer.render(parsedTitle);
+  				let parsedBody = reader.parse(resContent[i].body);
+  				let curBody = writer.render(parsedBody);
+  				resTitle.push(curTitle);	
+  				resBody.push(curBody);
+	  		}
 
-				if(resContent.length > 5){
-					next_id = resContent[5].postid;
-				}
-
-	  			res.render('blogsList', { username: givenUsername, nextId: next_id, title: resTitle, body: resBody });
+			if(resContent.length > 5){
+				next_id = resContent[5].postid;
 			}
 
-  		});
-  		client.close();
+	  		res.render('blogsList', { username: givenUsername, nextId: next_id, title: resTitle, body: resBody });
+		}
 
 	});
 })
