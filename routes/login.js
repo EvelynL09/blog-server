1f79aaaf4622fe1935404e1d9a7405d2cfad8074
EmilyLiu18 <tong9718@gmail.com>
2020-05-08 20:04:13 -0700

    partial login.js updated

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,20 @@
 let express = require('express');
 let router = express.Router();
 let bcrypt = require('bcryptjs');
+let client = require('../db');
 
 
-module.exports = router;
+router.get('/', function (req, res) {
+	let givenUsername = req.query.username;
+	let givenPassword = req.query.password;
+	let givenRedirect = req.query.redirect;
+
+})
+
+
+router.post('/', function (req, res){
+	let givenUsername = req.body.username;
+	let givenPassword = req.body.password;
+	let givenRedirect = req.body.redirect;
+
+})
