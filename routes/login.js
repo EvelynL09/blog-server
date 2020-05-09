04fc72d91207410fee4a2b4dc50ac0f647d2a8fd
EmilyLiu18 <tong9718@gmail.com>
2020-05-08 21:32:50 -0700

    hash wrong

--- routes/login.js
+++ routes/login.js
@@ -17,7 +17,53 @@ router.post('/', function (req, res, next){
 	let givenUsername = req.body.username;
 	let givenPassword = req.body.password;
 	let givenRedirect = req.body.redirect;
-	res.send(givenRedirect);
+	let collection = client.dbCollection('BlogServer', 'Users');
+
+	
+	collection.findOne({"username":givenUsername}, function(err, resContent) {
+  		if(err){
+  			throw err;
+  		}
+  		if(resContent == null){
+			res.status(401);
+			//TODO: check if needed parameters
+			res.render('login');
+  		}
+		else{
+			let dbPassword = resContent.password;
+
+			bcrypt.genSalt(10, function(err, salt) {
+    			bcrypt.hash("password", salt, function(err, hash) {
+    				res.send(hash);
+    				/*
+        			// Store hash in your password DB.
+        			bcrypt.compare(dbPassword, hash, function(err, ifMatched) {
+						if(err){
+							throw err;
+						}
+
+						// password correct. success
+						if(ifMatched){
+							res.send("Correct password" + "\n" + dbPassword+"\n"+ hash);
+
+						}
+						// unsuccess
+						else{
+							res.send("unsuccess"+ "\n" + dbPassword+"\n"+ hash);
+
+						}
+
+
+					});*/
+    			});
+			});
+
+			
+
+		}
+
+
+	});
 })
 
 module.exports = router;
